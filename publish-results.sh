336ef396002cb576374cc69d1e90d6d9c50467ef
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2012-11-15 18:11:57 +0100

    publish-results: list files (for debug)

--- publish-results.sh
+++ publish-results.sh
@@ -2,6 +2,7 @@
 
 if [ -d build/downloads ]; then
 	pushd build/downloads
+		ls -l
 		rm *.done
 		rm -rf git2 svn cvs bzr # we publish files not SCM dirs
 	popd
@@ -10,8 +11,11 @@ fi
 if [ -d build/sstate-cache/ ]; then
 	pushd build/sstate-cache/
 		rm -f `find . -type l`
+		ls -l
 		mv */* .
+		ls -l
 		mv Ubuntu-*/*/* .
+		ls -l
 		rm -rf ?? Ubuntu-*
 	popd
 fi
