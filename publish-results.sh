c2a505118d4909ddedc422897af9cb9107770609
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2012-11-16 16:29:55 +0100

    publish-results: do nothing (will switch to better way)

--- publish-results.sh
+++ publish-results.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-if [ -d build/downloads ]; then
-	pushd build/downloads
-		rm *.done
-		rm -rf git2 svn cvs bzr aarch64 # we publish files not SCM dirs
-	popd
-fi
-
-if [ -d build/sstate-cache/ ]; then
-	pushd build/sstate-cache/
-		rm -f `find . -type l`
-		mv */* .
-		mv Ubuntu-*/*/* .
-		rm -rf ?? Ubuntu-*
-	popd
-fi
