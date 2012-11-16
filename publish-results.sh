6f3b11cd433ce86634b0e0e60c95dcc7251e6646
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2012-11-16 15:01:25 +0100

    publish-results: be quiet

--- publish-results.sh
+++ publish-results.sh
@@ -2,20 +2,16 @@
 
 if [ -d build/downloads ]; then
 	pushd build/downloads
-		ls -l
 		rm *.done
-		rm -rf git2 svn cvs bzr # we publish files not SCM dirs
+		rm -rf git2 svn cvs bzr aarch64 # we publish files not SCM dirs
 	popd
 fi
 
 if [ -d build/sstate-cache/ ]; then
 	pushd build/sstate-cache/
 		rm -f `find . -type l`
-		ls -l
 		mv */* .
-		ls -l
 		mv Ubuntu-*/*/* .
-		ls -l
 		rm -rf ?? Ubuntu-*
 	popd
 fi
