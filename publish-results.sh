8723f9a92066a8b63643cf7ba029a34c72566bf4
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2012-12-06 15:00:25 +0100

    publish-results: provide manifests for images

--- publish-results.sh
+++ publish-results.sh
@@ -5,3 +5,10 @@ mkdir -p ${WORKSPACE}/downloads
 cp /mnt/ci_build/workspace/downloads/* ${WORKSPACE}/downloads
 rm ${WORKSPACE}/downloads/*.done
 
+# provide manifest for images
+cd build/tmp-eglibc/deploy/images/
+for img in *v8-*ext2.gz
+do
+	img=`basename $img .rootfs.ext2.gz`
+	cp ../licenses/$img/license.manifest $img.manifest
+done
