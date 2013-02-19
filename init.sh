bc91b518db0ce525aad81c37cc68b2401310a4db
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2013-02-19 22:48:16 +0100

    init: check $1 properly
    Signed-off-by: Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>

--- init.sh
+++ init.sh
@@ -119,7 +119,7 @@ echo 'DISTRO_FEATURES = "x11 alsa argp ext2 largefile usbgadget usbhost xattr nf
 
 sed -i -e "s/^MACHINE.*//g" conf/local.conf
 
-if [ -n $1 ];
+if [ -n "$1" ];
 then
 	bitbake $1
 fi
