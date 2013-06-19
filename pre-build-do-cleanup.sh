c9e9630494687f14c4ea91d9e569abe71155ac02
Fathi Boudra <fathi.boudra@linaro.org>
2013-06-19 10:23:18 +0300

    pre-build-do-cleanup.sh: remove a few hardcoded path
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- pre-build-do-cleanup.sh
+++ pre-build-do-cleanup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# clean builds from all jobs to get disk space back
-# path is something like that: openembedded-armv8-rootfs/label/oe_persistent_cloud/rootfs/lamp/
-rm -rf /mnt/ci_build/workspace/openembedded-armv8-rootfs/label/*/*/*/build
-rm -rf /mnt/ci_build/workspace/openembedded-armv7a-lamp/gcc_version/*/label/*/build
+if [ -n "${WORKSPACE}" ]; then
+    # clean builds from all jobs to get disk space back
+    find ${WORKSPACE} -type d -name build | xargs rm -rf
 
-# those should not exist but they may
-rm -rf /mnt/ci_build/workspace/openembedded-armv8-rootfs/label/*/*/*/downloads
-rm -rf /mnt/ci_build/workspace/openembedded-armv7a-lamp/gcc_version/*/label/*/downloads
+    # those should not exist but they may
+    find ${WORKSPACE} -type d -name downloads | xargs rm -rf
 
-# || true as some of those dirs may not exist
-du -hs /mnt/ci_build/workspace/openembedded-armv8-rootfs/label/ \
-       /mnt/ci_build/workspace/openembedded-armv7a-lamp/gcc_version/ \
+    # || true as some of those dirs may not exist
+    du -hs ${WORKSPACE} \
        /mnt/ci_build/workspace/downloads \
        /mnt/ci_build/workspace/sstate-cache || true
-df -h
+    df -h
+fi
