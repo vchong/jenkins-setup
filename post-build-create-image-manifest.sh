a02721dabe720897eb83f67d531ed515845709ec
Fathi Boudra <fathi.boudra@linaro.org>
2013-06-20 12:40:41 +0300

    Get rid of hardcoded deploy dir Move the files to out dir (previously in the CI job)
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- post-build-create-image-manifest.sh
+++ post-build-create-image-manifest.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# provide manifest for images
-cd build/tmp-eglibc/deploy/images/
-for img in *.tar.gz
-do
-	if ! [ -h $img ] ; then
-		img=`basename $img .rootfs.tar.gz`
-		cp ../licenses/$img/license.manifest $img.manifest
-	fi
-done
+if [ -n "${WORKSPACE}" ]; then
+	test -d ${WORKSPACE}/out || mkdir -p ${WORKSPACE}/out
+	rm -rf ${WORKSPACE}/out/*
+	deploy_dir=`find build -type d -name deploy`
+	cd ${deploy_dir}/images
+	for img in *.rootfs.tar.gz
+	do
+		if ! [ -h $img ] ; then
+			img=`basename $img .rootfs.tar.gz`
+			cp -a ../licenses/$img/license.manifest $img.manifest
+			mv $img.* ${WORKSPACE}/out
+		fi
+	done
+fi
