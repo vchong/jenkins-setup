bae2b7df511c476636636000fac527f888345a15
Riku Voipio <riku.voipio@linaro.org>
2013-07-02 10:58:56 +0300

    periodic cleanup: enable hard cleanup
    enable hard cleanup when less then 20% of persistent diskspace is
    available

--- periodic-cleanup.sh
+++ periodic-cleanup.sh
@@ -25,6 +25,14 @@ shift $(( OPTIND-1 ))
 cd openembedded-core
 . oe-init-build-env ../build
 
+diskspace=`df -h $WORKBASE|tail -n1`
+used=`echo $diskspace | awk '{ print $5}' | cut -d'%' -f1  `
+if [ $used -ge 80 ]; then
+    echo "more then 80% of disk used, hard cleanup"
+    echo $diskspace
+    job=hard
+fi
+
 case $job in
     soft)
         cleanup_soft
