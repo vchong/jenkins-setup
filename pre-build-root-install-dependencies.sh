f2651e7487fb6404a8fbc034dc58a8cc30601885
Fathi Boudra <fathi.boudra@linaro.org>
2013-08-01 12:25:19 +0300

    Add Fedora support and clean up dependencies based on Yocto Project Reference Manual.
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- pre-build-root-install-dependencies.sh
+++ pre-build-root-install-dependencies.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y --force-yes sed wget cvs subversion git bzr coreutils \
-		unzip bzip2 tar gzip cpio gawk python patch diffstat make \
-		build-essential gcc g++ desktop-file-utils chrpath autoconf automake \
-		libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev texi2html texinfo \
-		realpath
+DISTRIBUTION=`grep '^ID=' /etc/os-release | sed 's/ID=//'`
+if [ "$DISTRIBUTION" = "ubuntu" ]; then
+  apt-get update
+  # Package list based on Yocto Project Reference Manual
+  # http://www.yoctoproject.org/docs/current/ref-manual/ref-manual.html
+  # Essentials
+  apt-get install -y --force-yes gawk wget git-core diffstat unzip texinfo \
+    build-essential chrpath
+  # Graphical Extras
+  #apt-get install -y --force-yes libsdl1.2-dev xterm
+  # Documentation
+  #apt-get install -y --force-yes make xsltproc docbook-utils fop
+  # ADT Installer Extras
+  #apt-get install -y --force-yes autoconf automake libtool libglib2.0-dev
+  # Linaro Extras
+  apt-get install -y --force-yes automake bzip2 bzr cvs desktop-file-utils \
+    libtool patch python realpath subversion texi2html
+elif [ "$DISTRIBUTION" = "fedora" ]; then
+  # Essentials
+  yum install gawk make wget tar bzip2 gzip python unzip perl patch \
+    diffutils diffstat git cpp gcc gcc-c++ eglibc-devel texinfo chrpath \
+    ccache
+  # Graphical Extras
+  #yum install SDL-devel xterm
+  # Documentation
+  #yum install make docbook-style-dsssl docbook-style-xsl \
+  #  docbook-dtds docbook-utils fop libxslt
+  # ADT Installer Extras
+  #yum install autoconf automake libtool glib2-devel
+done
 
 # Get latest repo script and install it in PATH (use our mirror of repo)
 test -d $HOME/bin || mkdir -p $HOME/bin
