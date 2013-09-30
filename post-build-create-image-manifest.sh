8f2e90f9c2744557061733f7f467e0de11bae509
Riku Voipio <riku.voipio@linaro.org>
2013-09-30 12:19:04 +0300

    fix image manifests for lamp sysroot

--- post-build-create-image-manifest.sh
+++ post-build-create-image-manifest.sh
@@ -7,9 +7,12 @@ if [ -n "${WORKSPACE}" ]; then
 	source ${oe_init_build_env} build
 	deploy_dir_image=`bitbake -e | grep "^DEPLOY_DIR_IMAGE="| cut -d'=' -f2 | tr -d '"'`
 	license_directory=`bitbake -e | grep "^LICENSE_DIRECTORY="| cut -d'=' -f2 | tr -d '"'`
-	license_manifest=`find ${license_directory} -type f -name 'license.manifest'`
-	image_name=`dirname ${license_manifest}`
-	image_name=`basename ${image_name}`
-	cp -a ${license_manifest} ${WORKSPACE}/out/${image_name}.manifest
-	mv ${deploy_dir_image}/${image_name}.* ${WORKSPACE}/out
+	license_manifests=`find ${license_directory} -type f -name 'license.manifest'`
+	for manifest in ${license_manifests}
+	do
+		image_name=`dirname ${manifest}`
+		image_name=`basename ${image_name}`
+		cp -a ${manifest} ${WORKSPACE}/out/${image_name}.manifest
+		mv ${deploy_dir_image}/${image_name}.* ${WORKSPACE}/out
+	done
 fi
