bfb7a0afa7373c20694175540ec46c4df1ec4f34
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2013-12-16 21:53:23 +0100

    Revert "add buildstats content in buildhistory tree"
    This reverts commit 4240cd6f062b5895fbfe6807bc07edd23aaa74a9.
    
    Adding the buildstats creates more noise than anything else after all, between
    two builds most of the builstats content change, and it clutters the commit diff
    and history.
    
    Change-Id: I36e5ba1734b1c886d93815ef5f37f92a6cb2e607

--- post-build-manage-buildhistory.sh
+++ post-build-manage-buildhistory.sh
@@ -53,26 +53,10 @@ if [ ! -d $buildhistory_dir ]; then
 	buildhistory_dir=`find /mnt/ci_build/workspace/tmp -type d -name buildhistory`
 fi
 
-buildstats_dir=`find build -maxdepth 2 -type d -name buildstats`
-if [ ! -d $buildstats_dir ]; then
-	buildstats_dir=`find /mnt/ci_build/workspace/tmp -type d -name buildstats`
-fi
-
 if [ ! -z "$buildhistory_dir" ] && [ -d $buildhistory_dir ]; then
     rm -rf _buildhistory
     git clone $tree -b $branch _buildhistory
     cp -a $buildhistory_dir/* _buildhistory/
-
-    # also copy buildstats if they exist
-    if [ ! -z "$buildstats_dir" ] && [ -d $buildstats_dir ]; then
-        mkdir -p _buildhistory/buildstats
-        # buildstats has this layout:
-        #  buildstats/<build-name>-<machine>/<timestamp>/<recipe-PF>/stats
-        # we want to copy only the leaf folders with the stats, otherwise the commit diff
-        # will be hard to read (if it includes the timestamps which changes everyday)
-        find $buildstats_dir/*/*/* -type d | xargs -i cp -a {} _buildhistory/buildstats/
-    fi
-
     cd _buildhistory
     git add -A
     git commit --allow-empty -m "Build : ${BUILD_NUMBER}"
